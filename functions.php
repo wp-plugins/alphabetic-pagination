dc6a992db5b0c89d8ebd6cb065b7b6959e352fa8
fahadmahmood <fahadmahmood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-03 20:07:28 +0000

    Activate Plugin - Default settings Upper Case & Horizontal Layout
    git-svn-id: https://plugins.svn.wordpress.org/alphabetic-pagination/tags/0.2@722240 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,315 @@
+<?php
+
+
+
+	
+
+	//FOR QUICK DEBUGGING
+
+
+
+
+
+	if(!function_exists('pre'))
+
+
+
+
+
+	{
+
+
+
+		function pre($data)
+
+
+
+
+
+		{
+
+
+
+			echo '<pre>';
+
+
+
+			print_r($data);
+
+
+
+			echo '</pre>';	
+
+
+
+		}	 
+
+
+
+	}
+
+
+
+
+
+	if(!function_exists('ap_start'))
+
+
+
+
+
+	{
+
+
+
+
+
+		function ap_start()
+
+
+
+
+
+		{	
+
+
+
+				
+				update_option( 'ap_case', 'U');
+				update_option( 'ap_layout', 'H');
+
+
+		}	
+
+
+
+
+
+	}
+
+	if(!function_exists('ap_end'))
+
+
+
+
+
+	{
+
+
+
+		function ap_end()
+
+
+
+
+
+		{	
+
+				delete_option( 'ap_case');
+				delete_option( 'ap_layout');
+
+		}
+
+
+
+		
+
+
+
+	}	
+
+	
+
+
+
+
+
+	
+
+	if(!function_exists('ap_pagination'))
+
+	{
+
+		function ap_pagination()
+
+		{
+
+			global $wpdb;
+
+			
+
+			if(!is_admin() && is_main_query())// && $wpdb->num_rows>0)
+
+			{
+
+				if(!function_exists('ap_where'))
+
+				{
+
+					
+
+					function ap_where($where)
+
+					{
+
+						global $wpdb;
+
+						$where .= ' AND '.$wpdb->prefix.'posts.post_title LIKE "'.mysql_real_escape_string($_GET['ap']).'%"';
+
+						return $where;
+
+					}
+
+				}
+
+			
+
+					
+
+				add_filter( 'posts_where' , 'ap_where' );	
+
+				
+
+				if(!function_exists('render_alphabets')){
+
+					
+
+					function render_alphabets(){
+
+							$default_place = get_option('ap_dom')==''?'#content':get_option('ap_dom');
+
+							 
+
+							$alphabets = ap_alphabets();
+
+							
+
+							$alphabets_bar = '<ul class="ap_pagination case_'.get_option('ap_case').' layout_'.get_option('ap_layout').'">';
+
+							
+
+							foreach($alphabets as $alphabet){
+
+								$alphabets_bar .= '<li>';
+
+								$alphabets_bar .= '<a href="'.add_query_arg( array('ap' => $alphabet), $_SERVER["REQUEST_URI"]).'" class="'.(strtolower($_GET['ap'])==$alphabet?'selected':'').'">'.$alphabet.'</a>';
+
+								$alphabets_bar .= '</li>';
+
+							}
+
+							
+
+							$alphabets_bar .= '</ul>';
+
+							
+
+							$script = '<script type="text/javascript" language="javascript">jQuery(document).ready(function($) {
+
+				// Handler for .ready() called.
+
+				jQuery("'.$default_place.'").prepend(\''.$alphabets_bar.'\');
+
+					
+
+				});';
+
+							
+
+							if(get_option('ap_layout')=='V'){
+
+								
+
+								$script .= '
+
+								setTimeout(function(){
+
+								var p = jQuery("'.$default_place.'");
+
+								
+
+								var position = p.position();
+
+								
+
+										jQuery(".layout_V").css({left:position.left-26}); }, 100);';
+
+								
+
+							}
+
+							
+
+							
+
+				$script .= '
+
+					
+
+					
+
+			</script>';
+
+							
+
+							
+
+							echo $script;
+
+					}
+
+				}
+
+				
+
+				
+
+				add_action("wp_footer", 'render_alphabets');
+
+				
+
+			}
+
+		}
+
+	}
+
+	
+
+	
+
+	if(!function_exists('ap_alphabets'))
+
+	{
+
+		function ap_alphabets()
+
+		{
+
+			//LETS START WITH AN OLD STRING
+
+			
+
+			$str = "quick brown fox jumps over the lazy dog";
+
+			
+
+			$alpha_array = array_unique(str_split(str_replace(' ', '', $str)));
+
+			asort($alpha_array);
+
+			
+
+			$alphabets = array_values($alpha_array);
+
+			
+
+			return $alphabets;
+
+			
+
+		}
+
+	}
+
+?>
