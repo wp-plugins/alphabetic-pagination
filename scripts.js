9fc3449b48f789baab33dcf9628d408192fbbed5
fahadmahmood <fahadmahmood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-29 06:48:44 +0000

    $ap_langs = is_array($ap_langs)?$ap_langs:array();
    git-svn-id: https://plugins.svn.wordpress.org/alphabetic-pagination/tags/1.2.3@847510 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scripts.js
@@ -0,0 +1,46 @@
+// JavaScript Document
+jQuery(document).ready(function(){
+								
+	var ap_methods = {
+		load_sub_items: function(type){
+			
+			jQuery.post(ajaxurl, {action: 'ap_tax_types',"type":type}, function(response) {
+				response = jQuery.parseJSON(response);																
+				if(response.msg){
+					var data = response.data;
+					var selected = response.selected;
+					var items = '';
+					jQuery.each(data, function(i, v){
+						var is_selected = '';											   
+						jQuery.each(selected, function(is, vs){
+							
+							
+							if(vs==i)
+							is_selected = 'selected="selected"';
+							
+							
+							
+						});
+						
+						items+='<option value="'+i+'" '+is_selected+'>'+v+'</option>';
+					
+						
+					});
+					jQuery('#tax_types_selector').html(items);
+					jQuery('div.ap_tax_types').show();
+
+				}																
+				
+			});
+		}
+	}
+	
+	jQuery('#tax_selector').live('change',function(){
+		var type = jQuery(this).val();				
+		if(type.length>0)
+		ap_methods.load_sub_items(type);
+
+	  
+   	});
+	
+});
