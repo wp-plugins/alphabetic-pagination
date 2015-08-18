addbfc2b36e140ecec7e4168a1c2c2d82ea2d2b9
fahadmahmood <fahadmahmood@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-18 12:35:57 +0000

    releasing debugged version
    git-svn-id: https://plugins.svn.wordpress.org/alphabetic-pagination/tags/2.1.1@1223631 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php 
+/*
+
+
+
+Plugin Name: Alphabetic Pagination
+
+
+
+Plugin URI: http://www.websitedesignwebsitedevelopment.com/wordpress/plugins/alphabetic-pagination
+
+
+
+Description: Alphabetic pagination is a great plugin to filter your posts/pages and WooCommerce products with alphabets. It is compatible with custom taxonomies.
+
+
+
+Version: 2.1
+
+
+
+Author: Fahad Mahmood 
+
+
+
+Author URI: http://www.androidbubbles.com
+
+
+
+License: GPL3
+
+
+
+*/ 
+
+
+        
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        
+
+	global $ap, $arg, $ap_implementation, $rendered, $premium_link, $ap_data, $ap_dir, $alphabets_bar, $ap_custom, $ap_queries, $ap_query, $css_arr, $disabled_letters, $ap_current_cat, $ap_group;
+	$disabled_letters = array();
+	$arg = 'ap';
+	$ap_dir = plugin_dir_path( __FILE__ );
+	$css_arr = array('');
+	$ap_custom = file_exists($ap_dir.'inc/functions_extended.php');
+	$ap_queries = 0;
+	$ap_query = false;
+	$rendered = FALSE;
+	$premium_link = 'http://shop.androidbubbles.com/product/alphabetic-pagination-pro';
+	define('AP_CUSTOM', 'custom');
+	$ap_group = (get_option('ap_grouping')==0?false:true);
+	$ap_data = get_plugin_data(__FILE__);
+	$ap = isset($_GET[$arg])?$_GET[$arg]:'';
+	
+	
+	include('inc/functions.php');
+        
+	register_activation_hook(__FILE__, 'ap_start');
+
+	//KBD END WILL REMOVE .DAT FILES	
+
+	register_deactivation_hook(__FILE__, 'ap_end' );
+
+
+
+	add_action( 'admin_enqueue_scripts', 'register_ap_scripts' );
+	add_action( 'wp_enqueue_scripts', 'register_ap_scripts' );
+	
+
+
+	//add_filter('found_posts_query', 'ap_pagination', 1);
+
+	//pre_get_posts
+	
+	
+	if(is_admin()){
+		add_action( 'admin_menu', 'ap_menu' );	
+		add_action( 'wp_ajax_ap_tax_types', 'ap_tax_types_callback' );
+		$plugin = plugin_basename(__FILE__); 
+		add_filter("plugin_action_links_$plugin", 'ap_plugin_links' );	
+		
+		if($ap_custom)
+		add_action( 'admin_enqueue_scripts', 'ap_pro_admin_style', 99 );
+		
+	}else{
+		
+		add_action('wp_head', 'ap_init_actions');
+		$ap_implementation = get_option('ap_implementation');
+		
+		
+		
+		if($ap_implementation=='' || $ap_implementation=='auto'){
+			add_filter('pre_get_posts', 'ap_pagination', 1);
+			//WILL WORK FROM SETTINGS
+			add_action('wp_footer', 'ap_ready');
+		}
+		if($ap_custom){		
+			include($ap_dir.'inc/functions_extended.php');
+		}
+		
+		
+	}
+
+
+	
